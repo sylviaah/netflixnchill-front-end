914adb3765308a06cbb818c6cbb37e3c0d4afd26
Your Name <you@example.com>
2021-01-31 20:17:47 -0800

    branch created

---
+++ front-end/comps/Inputs/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const Inputs = () => {
+    return<Container>
+
+
+    </Container>
+}
+
+Inputs.defaultProps = {
+
+}
+
+export default Inputs;
